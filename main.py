06822974d2ce5522032bbfcc95f470a4481d5f91
ABCwest28 <ABCwest28@gmail.com>
2022-12-29 21:40:14 +0500

    2nd commit

--- main.py
+++ main.py
@@ -43,6 +43,47 @@ class WidgetToDo(QMainWindow):
         self.set_font()
         self.show()
 
+    def layout_sizePol_init(self):
+        self.vbox.setSpacing(10)
+        self.vbox.addWidget(self.table)
+        self.vbox.addLayout(self.h1box)
+        self.h1box.addWidget(self.task_line)
+        self.h1box.addWidget(self.task_date)
+        self.h1box.addWidget(self.task_btn)
+        self.h1box.addWidget(self.sort_btn)
+        self.h1box.addWidget(self.sort_combo)
+        #self.h1box.addWidget(self.sort_u_btn)
+        self.setLayout(self.vbox)
+
+        self.sizePolicy_task_date = QSizePolicy(QSizePolicy.Minimum, QSizePolicy.Fixed)
+        self.sizePolicy_task_date.setHorizontalStretch(30)
+        self.task_date.setSizePolicy(self.sizePolicy_task_date)
+
+        self.sizePolicy_task_line = QSizePolicy(QSizePolicy.Minimum, QSizePolicy.Fixed)
+        self.sizePolicy_task_line.setHorizontalStretch(100)
+        self.task_line.setSizePolicy(self.sizePolicy_task_line)
+
+    def enable_disable_button(self):
+        if self.task_line.text():
+            self.task_btn.setEnabled(True)
+        else:
+            self.task_btn.setEnabled(False)
+
+    def table_init(self):
+        self.table.setColumnCount(4)
+        self.table.setHorizontalHeaderLabels(["Номер", "Задача", "Дата", "Статус"])
+
+        #self.table.horizontalHeader().setStretchLastSection(True)
+        self.table.horizontalHeader().setSectionResizeMode(1, QHeaderView.Stretch)
+        self.table.horizontalHeader().setSectionResizeMode(2, QHeaderView.Stretch)
+        self.table.horizontalHeader().setSectionResizeMode(3, QHeaderView.ResizeToContents)
+        self.table.horizontalHeader().setSectionResizeMode(0, QHeaderView.ResizeToContents)
+        self.table.verticalHeader().setVisible(False)
+
+        self.table.cellDoubleClicked.connect(self.rowSelected)
+
+        self.table.setRowCount(0)
+
 
 if __name__ == '__main__':
     app = QApplication(sys.argv)
